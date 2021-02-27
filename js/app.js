a8fd6980f78280710f9af82ced977d422bf6f909
Harish Swaminathan <harishswami62@gmail.com>
2021-02-27 13:22:04 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let gameSection = document.querySelector("#game-section");
 let playerDetails = document.querySelector(".player-details");
 
 let formButton = document.getElementById("button");
-
+let playerName;
 // form validation starts
 formButton.addEventListener("click", () => {
   let uname = document.getElementById("username").value;
@@ -18,7 +18,7 @@ formButton.addEventListener("click", () => {
   } else {
     sessionStorage.setItem("game", "true");
     sessionStorage.setItem("playername", uname);
-    let playerName = sessionStorage.getItem("playername");
+    playerName = sessionStorage.getItem("playername");
 
     form.style.display = "none";
     gameSection.style.display = "flex";
@@ -44,32 +44,85 @@ home.addEventListener("click", () => {
   gameSection.style.display = "none";
 });
 
-let buttons = document.querySelectorAll(".controls span");
+let buttons = document.querySelectorAll(".p-controls span");
 let imgCont = document.querySelector("#left-player");
+let imgContRight = document.querySelector("#right-computer");
 
 buttons.forEach((button, index) => {
   button.addEventListener("click", () => {
-    let bigImg = document.createElement("img");
-    let images = [
-      "../assets/rock.png",
-      "../assets/paper.png",
-      "../assets/scissors.png",
+    let randomPlay = Math.floor(Math.random() * 10);
+    console.log(randomPlay);
+
+    player(index);
+    // jarvis(randomPlay);
+
+    while (randomPlay > 2) {
+      randomPlay = Math.floor(randomPlay / 2);
+    }
+    // console.log(randomPlay);
+    let bigImgRight = document.createElement("img");
+    let imagesRight = [
+      "../assets/rock-right.png",
+      "../assets/paper-right.png",
+      "../assets/scissors-right.png",
     ];
 
-    buttons.forEach((button) => {
-      button.style.pointerEvents = "none";
-    });
+    imgContRight.appendChild(bigImgRight);
+    bigImgRight.setAttribute("src", imagesRight[randomPlay]);
+    bigImgRight.setAttribute("class", "big-img-right");
+    bigImgRight.style.animation = "bigimageRight 5s ease";
 
-    imgCont.appendChild(bigImg);
-    bigImg.setAttribute("src", images[index]);
-    bigImg.setAttribute("class", "big-img");
-    bigImg.style.animation = "bigimage 5s ease";
-
-    bigImg.addEventListener("animationend", () => {
-      imgCont.removeChild(bigImg);
-      buttons.forEach((button) => {
-        button.style.pointerEvents = "";
-      });
+    bigImgRight.addEventListener("animationend", () => {
+      imgContRight.removeChild(bigImgRight);
     });
+    console.log(randomPlay);
+
+    if (index === randomPlay) {
+      setTimeout(() => {
+        alert("you tie");
+      }, 5000);
+    } else if (
+      (index == 0 && randomPlay == 2) ||
+      (index == 1 && randomPlay == 0) ||
+      (index == 2 && randomPlay == 1)
+    ) {
+      setTimeout(() => {
+        alert("you win");
+      }, 5000);
+    } else if (
+      (index == 0 && randomPlay == 1) ||
+      (index == 1 && randomPlay == 2) ||
+      (index == 2 && randomPlay == 0)
+    ) {
+      setTimeout(() => {
+        alert("you lose");
+      }, 5000);
+    }
   });
 });
+
+// player functionalities
+function player(index) {
+  let bigImg = document.createElement("img");
+  let images = [
+    "../assets/rock.png",
+    "../assets/paper.png",
+    "../assets/scissors.png",
+  ];
+
+  buttons.forEach((button) => {
+    button.style.pointerEvents = "none";
+  });
+
+  imgCont.appendChild(bigImg);
+  bigImg.setAttribute("src", images[index]);
+  bigImg.setAttribute("class", "big-img");
+  bigImg.style.animation = "bigimage 5s ease";
+
+  bigImg.addEventListener("animationend", () => {
+    imgCont.removeChild(bigImg);
+    buttons.forEach((button) => {
+      button.style.pointerEvents = "";
+    });
+  });
+}
