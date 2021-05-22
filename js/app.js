e8fe20a2b368ab0d1a4316fba21385a97685eb13
Harish Swaminathan <harishswami62@gmail.com>
2021-05-22 20:57:01 +0530

    updated final

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 let gameSection = document.querySelector("#game-section");
-
 let playerDetails = document.querySelector(".player-details");
 let leftPlayer = document.querySelector(".Player-name");
 let formButton = document.getElementById("button");
@@ -9,6 +8,7 @@ let refresh = document.querySelector(".refresh");
 refresh.addEventListener("click", () => {
   location.reload();
 });
+
 // form validation starts
 formButton.addEventListener("click", () => {
   let uname = document.getElementById("username").value;
@@ -61,25 +61,22 @@ let pointGain = 0;
 let pointlose = 0;
 buttons.forEach((button, index) => {
   button.addEventListener("click", () => {
-    let randomPlay = Math.floor(Math.random() * 10);
+    let randomPlay = Math.floor(Math.random() * 3);
     console.log(randomPlay);
 
     player(index);
 
-    while (randomPlay > 2) {
-      randomPlay = Math.floor(randomPlay / 2);
-    }
     let bigImgRight = document.createElement("img");
     let imagesRight = [
-      "../assets/rock-right.png",
-      "../assets/paper-right.png",
-      "../assets/scissors-right.png",
+      "./assets/rock-right.png",
+      "./assets/paper-right.png",
+      "./assets/scissors-right.png",
     ];
 
     imgContRight.appendChild(bigImgRight);
     bigImgRight.setAttribute("src", imagesRight[randomPlay]);
     bigImgRight.setAttribute("class", "big-img-right");
-    bigImgRight.style.animation = "bigimageRight 5s ease";
+    bigImgRight.style.animation = "bigimageRight 3s ease";
 
     bigImgRight.addEventListener("animationend", () => {
       imgContRight.removeChild(bigImgRight);
@@ -92,30 +89,46 @@ buttons.forEach((button, index) => {
 
     if (index === randomPlay) {
       setTimeout(() => {
-        alert("you tie");
-      }, 5000);
+        // alert("you tie");
+        let message = `No one scored this round`;
+        popupFunction(message);
+      }, 3000);
     } else if (
       (index == 0 && randomPlay == 2) ||
       (index == 1 && randomPlay == 0) ||
       (index == 2 && randomPlay == 1)
     ) {
       setTimeout(() => {
-        alert("you gain a point");
-        dots[pointGain].style.background = "green";
+        // alert("you gain a point");
+        dots[pointGain].classList.add("greenpoint");
         pointGain++;
-        pointGain == 5 ? alert("you won the tournament......") : false;
-      }, 5000);
+        if (pointGain == 5) {
+          // alert("you won the tournament......");
+          let message = `${playerName} won this Tournament by scoring <span class='point'>${pointGain}</span> points`;
+          location.reload();
+        }
+        let message = `${playerName} scored  <span class='point'>${pointGain}</span> point`;
+        popupFunction(message);
+      }, 3000);
     } else if (
       (index == 0 && randomPlay == 1) ||
       (index == 1 && randomPlay == 2) ||
       (index == 2 && randomPlay == 0)
     ) {
       setTimeout(() => {
-        alert("you lose a point");
-        dotsAi[pointlose].style.background = "green";
+        // alert("you lose a point");
+        dotsAi[pointlose].classList.add("greenpoint");
         pointlose++;
-        pointlose == 5 ? alert("you lose the tournament......") : false;
-      }, 5000);
+        if (pointlose == 5) {
+          // alert("you lose the tournament......");
+          let message = `${playerName} lost this Tournament. Better luck! next time.`;
+
+          popupFunction(message);
+          location.reload();
+        }
+        let message = `${playerName} lost this round`;
+        popupFunction(message);
+      }, 3000);
     }
   });
 });
@@ -124,9 +137,9 @@ buttons.forEach((button, index) => {
 function player(index) {
   let bigImg = document.createElement("img");
   let images = [
-    "../assets/rock.png",
-    "../assets/paper.png",
-    "../assets/scissors.png",
+    "./assets/rock.png",
+    "./assets/paper.png",
+    "./assets/scissors.png",
   ];
 
   buttons.forEach((button) => {
@@ -136,7 +149,7 @@ function player(index) {
   imgCont.appendChild(bigImg);
   bigImg.setAttribute("src", images[index]);
   bigImg.setAttribute("class", "big-img");
-  bigImg.style.animation = "bigimage 5s ease";
+  bigImg.style.animation = "bigimage 3s ease";
 
   bigImg.addEventListener("animationend", () => {
     imgCont.removeChild(bigImg);
@@ -145,3 +158,14 @@ function player(index) {
     });
   });
 }
+
+let popupFunction = (message) => {
+  let popup = document.querySelector("#popup");
+  let popText = document.querySelector(".poptext");
+  let popBtn = document.querySelector(".popbtn");
+  popup.classList.add("block");
+  popText.innerHTML = `${message}`;
+  popBtn.addEventListener("click", () => {
+    popup.classList.remove("block");
+  });
+};
